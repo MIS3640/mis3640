75dbf7f5e786a0c066cdb587a47cb2685ca0f24c
Zhi Li <lzblack@users.noreply.github.com>
2021-01-28 21:51:29 -0500

    Finish session 04

---
+++ session04/type_demo.py
@@ -0,0 +1,12 @@
+""" 
+This is block of comments.
+A new line of comment...
+This is another comment.
+"""
+
+age = input('What is your age? \n')
+age = int(age)
+if age >= 21:
+    print('Yes, you can.')
+else:
+    print('No, you cannot.')
