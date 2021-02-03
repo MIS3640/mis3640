f7e66e9394b0d22ba90af881ee75a4f8579e700f
Zhi Li <lzblack@users.noreply.github.com>
2021-02-02 21:38:03 -0500

    Fix print function

--- temp.py
+++ temp.py
@@ -1,9 +1,1 @@
-c = input("How many copies are you purchasing? :") # c = number of copies purchased
-c = int(c)
-book_price = 24.95 * 0.6 * c
-if c == 0:
-    shipping_cost = 0
-else:
-    shipping_cost = 3 + 0.75 * (c-1)
-total_cost = book_price + shipping_cost
-print(f"Total cost = ${total_cost:.2f}")
+print('hello')
