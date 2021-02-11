ee9ef5ee8640d731291c8d06af967cd1bde1334b
Zhi Li <lzblack@users.noreply.github.com>
2021-02-11 15:49:40 -0500

    Update my_abs.py

--- session05/my_abs.py
+++ session05/my_abs.py
@@ -1,34 +1,3 @@
-# Exercise 3
-# def my_abs(number):
-#     """
-#     Print the absolute value of a number
-
-#     number: an integer or a floating point number
-#     """
-#     if number >= 0:
-#         print(number)
-#     else:
-#         print(-number)
-
-
-# my_abs(-10)
-
-# Exercise 4
-# def my_abs(number):
-#     """
-#     Return the absolute value of a number
-
-#     number: an integer or a floating point number
-#     """
-#     if number >= 0:
-#         return number
-#     else:
-#         return -number
-
-
-# print(my_abs(-10))
-
-# Exercise 5
 def my_abs_5(number):
     """
     Return the absolute value of a number
@@ -42,7 +11,6 @@ def my_abs_5(number):
             return -number
     else:
         print('I don\'t know how to solve this')
-        # raise TypeError
 
 
 # print(my_abs_5(-10))
