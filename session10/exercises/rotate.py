2f3dc8a59bcca5b55391d6187d48fd81e4e5b957
Zhi Li <lzblack@users.noreply.github.com>
2021-02-22 22:52:10 -0500

    Create rotate.py

---
+++ session10/exercises/rotate.py
@@ -0,0 +1,16 @@
+def rotate(word, n):
+    """
+    Take a string, WORD, and an integer, N as parameters, and return a new string that contains the letters from the original string, WORD, rotated by the given amount, N.
+    """
+    new_string = ''
+    for letter in word:
+        new_string += chr(ord(letter) + n)
+    return new_string
+
+
+def main():
+    print(rotate('abc', 2))
+
+
+if __name__ == '__main__':
+    main()
